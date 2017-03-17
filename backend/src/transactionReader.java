112e909c883d357f88dd474327dd3d3dcf33d90c
BrianJayD <brian.domingo@uoit.net>
2017-03-17 13:16:34 -0400

    [Modified] transactionReader.java and updateUser.java

--- backend/src/transactionReader.java
+++ backend/src/transactionReader.java
@@ -1,3 +1,54 @@
+import java.io.*;
+import java.util.*;
+
 class transactionReader {
+  private static String fileName;
+
+  public transactionReader(String file) {
+    fileName = file;
+  }
+
+	public static String getFileName() {
+		return fileName;
+	}
+
+	public static void setFileName(String fileName) {
+		transactionReader.fileName = fileName;
+	}
+
+  public ArrayList<String> readFile() {
+
+    ArrayList<String> trnactns = new ArrayList<String>();
+
+    try {
+      Scanner sc = new Scanner(new File(getFileName()));
+
+
+      while (sc.hasNextLine()) {
+        trnactns.add(sc.nextLine());
+      }
+
+      sc.close();
+    } catch (IOException e) {
+      System.err.println("File not found!");
+      e.printStackTrace();
+    }
+    return trnactns;
+  }
+
+  public String getTransNum(String t) {
+    char[] tChar = t.toCharArray();
+    char[] tNum = new char[2];
+
+    for (int i = 0; i < 2; i++) {
+      tNum[i] = (tChar[i]);
+    }
+
+    String tnString = new String(tNum);
+
+    return tnString;
+  }
+
   
+
 }
--- backend/src/updateUser.java
+++ backend/src/updateUser.java
@@ -1,3 +1,31 @@
+import java.io.*;
+import java.util.*;
+
 class updateUser {
-  
+    private static String fileName;
+
+    public updateUser(String file) {
+      fileName = file;
+    }
+
+    public String getFileName() {
+      return fileName;
+    }
+
+    public void createUser(String cUser) {
+      transactionReader userReader = new transactionReader("currentaccounts.txt");
+
+      char[] newUsr = cUser.toCharArray();
+      char[] usrInfo = new char[28];
+
+      for (int i = 3; i < 31; i++) {
+        usrInfo[i-3] = newUsr[i];
+      }
+
+      String added = new String(usrInfo);
+
+      System.out.println(added);
+
+
+    }
 }
