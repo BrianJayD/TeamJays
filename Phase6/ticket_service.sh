408ee1c55a806f6b4479b20e96c5f0eefde140cb
BrianJayD <brian.domingo@uoit.net>
2017-04-10 18:43:04 -0400

    [Added] script to run the front end and back end for user.

--- Phase6/ticket_service.sh
+++ Phase6/ticket_service.sh
@@ -8,6 +8,10 @@ cd ..
 
 javac -d ./ back/main.java back/transactionReader.java back/updateTickets.java back/updateUser.java
 
-./front/ticket-seller < input/daily_test.inp
+./front/ticket-seller
 
 java main
+
+> transaction_file.txt #reset transaction file to be empty
+
+rm *.class
