5f66aa5e550d7b3b9761f99ab94a6921f6b7fd58
BrianJayD <brian.domingo@uoit.net>
2017-04-10 17:15:55 -0400

    [Modified] compiles front-end and back-end before running. Runs front-end. Reads input/daily_test.inp for test commands. Then runs back-end for updating.

--- Phase6/ticket_service.sh
+++ Phase6/ticket_service.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
-./front/ticket-seller < ./input/daily_test.inp
+cd front
+
+make
+
+cd ..
+
+javac -d ./ back/main.java back/transactionReader.java back/updateTickets.java back/updateUser.java
+
+./front/ticket-seller < input/daily_test.inp
+
+java main
